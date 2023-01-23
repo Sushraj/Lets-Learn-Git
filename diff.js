2699ea43002b51cf67c4a139f81ca605bc48db23
Sushant Rajhans <Sushantrajhans2222@gmail.com>
2023-01-24 01:06:48 +0530

    diff file commit

---
+++ diff.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export default class diff extends Component {
+  static propTypes = {second: third}
+
+  render() {
+    return (
+      <div>diff</div>
+    )
+  }
+}
